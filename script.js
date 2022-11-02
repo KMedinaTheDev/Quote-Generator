20da2ac72f7c559c102086abf16b6d8eb1dac1d8
Wren <wren@Karens-MacBook-Pro.local>
2022-11-02 17:05:54 -0500

    added loader

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+  loading();
   // pick a radom quote from quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if author field is null and replace it with 'unknown'
@@ -23,11 +37,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set the quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiURL);
